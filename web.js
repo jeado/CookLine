7145b994824b5853df85e03828c1084163682b1d
James Pearce <jpearce@fb.com>
2012-03-22 20:08:22 +0800

    locale testing

--- web.js
+++ web.js
@@ -104,7 +104,7 @@ app.get('/dish/:dish', function(request, response) {
     data.url = objectUrl('dish', request.params.dish);
     data.dishes = dishes;
     data.app_id = app_id;
-    data.locale = request.params.fb_locale;
+    data.locale = request.query.fb_locale;
     response.render('object', data)
   } else {
     response.send(404);
@@ -119,7 +119,7 @@ app.get('/ingredient/:ingredient', function(request, response) {
     data.ingredients = null;
     data.dishes = dishes;
     data.app_id = app_id;
-    data.locale = request.params.fb_locale;
+    data.locale = request.query.fb_locale;
     response.render('object', data)
   } else {
     response.send(404);
